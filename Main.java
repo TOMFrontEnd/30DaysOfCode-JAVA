dd7e42a4a2c7b2f73095a53e00ed686a5a9a75bf
tom gu <tomgu7035@gmail.com>
2023-09-16 13:16:18 +1000

    new added

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// FIXME: 给Person增加构造方法:
+		Person ming = new Person("小明", 12);
+		System.out.println(ming.getName());
+		System.out.println(ming.getAge());
+	}
+}
+ class Person {
+
+			private String name;
+			private int age;
+			
+			public Person(String name, int age) {
+				this.name = name;
+				this.age = age;
+				}
+
+			public String getName() {
+				return name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+
+			public int getAge() {
+				return age;
+			}
+
+			public void setAge(int age) {
+				this.age = age;
+			}
+
+ }
+
+	
